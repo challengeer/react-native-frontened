55634bc66385cbabf9a53273ed9ca5dd8de778f9
tomas-sedlak <t.sedlak.24@gmail.com>
2025-01-19 13:49:30 +0100

    Added header component

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import { View } from "react-native";
+import Text from "@/components/Text";
+
+interface HeaderProps {
+    leftSection?: React.ReactNode;
+    title?: string;
+    rightSection?: React.ReactNode;
+}
+
+export default function Header({ leftSection, title, rightSection }: HeaderProps) {
+    return (
+        <View className="relative flex-row items-center justify-between gap-2 px-4 py-4">
+            <View>{leftSection}</View>
+            <Text className="absolute inset-x-0 text-center text-xl font-bold">{title}</Text>
+            <View>{rightSection}</View>
+        </View>
+    )
+}
