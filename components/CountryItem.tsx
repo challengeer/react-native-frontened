ae9a47673ab7a05bcb4f2137a4e3d39ab4155312
tomas-sedlak <t.sedlak.24@gmail.com>
2025-01-20 22:53:35 +0100

    Fixed flag size in country item

--- components/CountryItem.tsx
+++ components/CountryItem.tsx
@@ -10,7 +10,7 @@ interface CountryItemProps {
 export default function CountyItem({ flag, name, dial_code }: CountryItemProps) {
     return (
         <View className="py-4 flex-row items-center gap-4 border-b border-neutral-100 dark:border-neutral-800">
-            <Text className="text-[24px] mt-2">{flag}</Text>
+            <Text className="text-2xl">{flag}</Text>
             <View className="flex-1 flex-row items-center justify-between gap-2">
                 <Text>{name}</Text>
                 <Text type="secondary">{dial_code}</Text>
