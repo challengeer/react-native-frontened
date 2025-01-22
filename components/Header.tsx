8f34c828da602501db8e28d0782e6b7414c90685
tomas-sedlak <t.sedlak.24@gmail.com>
2025-01-22 22:16:13 +0100

    Fixed header title covering left and right section and making it unable to click

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header({ leftSection, title, rightSection }: HeaderProps
     return (
         <View className="relative flex-row items-center justify-between gap-2 px-4 py-4">
             <View>{leftSection}</View>
-            <Text className="absolute inset-x-0 text-center text-xl font-bold">{title}</Text>
+            <Text className="absolute inset-x-0 -z-10 text-center text-xl font-bold">{title}</Text>
             <View>{rightSection}</View>
         </View>
     )
