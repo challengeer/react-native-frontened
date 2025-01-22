f904c017260714c0242c10ed894facbf5606237f
mark-takac <steam.omg.mk@gmail.com>
2025-01-22 19:33:27 +0100

    Added InputBar component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,52 +1,52 @@
-import i18n from '@/i18n';
-import { useState } from 'react';
-import { FlatList, View } from 'react-native';
-import SearchBar from '@/components/SearchBar';
-import CountryItem from '@/components/CountryItem';
-import CountryInterface from '@/types/CountryInterface';
-import Header from '@/components/Header';
-import IconCircle from '@/components/IconCircle';
-import { XMarkIcon } from 'react-native-heroicons/outline';
+// import i18n from '@/i18n';
+// import { useState } from 'react';
+// import { FlatList, View } from 'react-native';
+// import SearchBar from '@/components/SearchBar';
+// import CountryItem from '@/components/CountryItem';
+// import CountryInterface from '@/types/CountryInterface';
+// import Header from '@/components/Header';
+// import IconCircle from '@/components/IconCircle';
+// import { XMarkIcon } from 'react-native-heroicons/outline';
 
-export default function HomeScreen() {
-  const countries = require("@/assets/data/countries.json")
-  const [searchQuery, setSearchQuery] = useState<string>("");
+// export default function HomeScreen() {
+//   const countries = require("@/assets/data/countries.json")
+//   const [searchQuery, setSearchQuery] = useState<string>("");
 
-  const filteredCountries = countries.filter((country: CountryInterface) =>
-    country.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    country.dial_code.includes(searchQuery)
-  );
+//   const filteredCountries = countries.filter((country: CountryInterface) =>
+//     country.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+//     country.dial_code.includes(searchQuery)
+//   );
 
-  return (
-    <>
-      <Header
-        title={i18n.t("countryPrefix.header")}
-        rightSection={
-          <IconCircle
-            icon={XMarkIcon}
-          />
-        }
-      />
+//   return (
+//     <>
+//       <Header
+//         title={i18n.t("countryPrefix.header")}
+//         rightSection={
+//           <IconCircle
+//             icon={XMarkIcon}
+//           />
+//         }
+//       />
 
-      <View className="px-4 gap-3">
-        <SearchBar
-          onSearch={setSearchQuery}
-        />
+//       <View className="px-4 gap-3">
+//         <SearchBar
+//           onSearch={setSearchQuery}
+//         />
 
-        <FlatList<CountryInterface>
-          overScrollMode="never"
-          showsVerticalScrollIndicator={false}
-          data={filteredCountries}
-          keyExtractor={(item: CountryInterface) => item.code}
-          renderItem={({ item }: { item: CountryInterface }) => (
-            <CountryItem
-              flag={item.flag}
-              name={item.name}
-              dial_code={item.dial_code}
-            />
-          )}
-        />
-      </View>
-    </>
-  );
-}
+//         <FlatList<CountryInterface>
+//           overScrollMode="never"
+//           showsVerticalScrollIndicator={false}
+//           data={filteredCountries}
+//           keyExtractor={(item: CountryInterface) => item.code}
+//           renderItem={({ item }: { item: CountryInterface }) => (
+//             <CountryItem
+//               flag={item.flag}
+//               name={item.name}
+//               dial_code={item.dial_code}
+//             />
+//           )}
+//         />
+//       </View>
+//     </>
+//   );
+// }
---
+++ components/InputBar.tsx
@@ -0,0 +1,64 @@
+import { useRef, useState } from "react";
+import { TextInput, View } from "react-native";
+import Text from "@/components/Text";
+import Icon from "@/components/Icon";
+import { XCircleIcon } from "react-native-heroicons/solid";
+
+
+interface InputBarProps {
+    onCancel?: () => void;
+    onSearch?: (query: string) => void;
+    label?: string;
+    description?: string;
+}
+
+export default function InputBar({ onCancel, onSearch, label, description }: InputBarProps) {
+    const inputRef = useRef<TextInput>(null);
+    const [focused, setFocused] = useState<boolean>(false);
+    const [value, setValue] = useState<string>("");
+
+    return (
+        <View className="flex-row items-center px-4 mt-10">
+            <View className="relative flex-1 flex-row items-center">
+                <Text className="absolute text-base text-neutral-800 dark:text-neutral-100 -top-7">{label}</Text>
+                <TextInput
+                    ref={inputRef}
+                    className="px-4 py-3 flex-1 bg-neutral-100 dark:bg-neutral-800 rounded-lg text-lg text-neutral-900 dark:text-neutral-100"
+                    value={value}
+                    onChangeText={(text) => {
+                        setValue(text);
+                    }}
+                    onFocus={() => setFocused(true)}
+                    onBlur={() => setFocused(false)}
+                />
+                {value && (
+                    <Icon
+                        icon={XCircleIcon}
+                        className="absolute right-4 z-10"
+                        lightColor="#737373"
+                        darkColor="#a3a3a3"
+                        onPress={() => {
+                            setValue("");
+                            onSearch?.("");
+                            inputRef.current?.blur();
+                        }}
+                    />
+                )}
+                <Text className="absolute top-14 text-sm text-neutral-500 dark:text-neutral-400">{description}</Text> {/* Maybe top-14 is not the best solution */}
+            </View>
+            {focused && (
+                <Text
+                    onPress={() => {
+                        setFocused(false);
+                        onCancel?.();
+                        inputRef.current?.blur();
+                    }}
+                    className="text-base text-neutral-900 dark:text-neutral-100 p-4 pr-0"
+                >
+                    Cancel
+                </Text>
+            )}
+
+        </View>
+    )
+}
