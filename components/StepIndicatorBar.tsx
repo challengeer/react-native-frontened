d31d2aa62e27de31634db048833a820e705c63a8
mark-takac <steam.omg.mk@gmail.com>
2025-01-22 21:20:31 +0100

    Fixed StepIndicator colors - light/dark mode support

--- components/StepIndicatorBar.tsx
+++ components/StepIndicatorBar.tsx
@@ -1,4 +1,5 @@
 import { View } from "react-native";
+import { useColorScheme } from "nativewind";
 
 interface StepIndicatorBarProps {
     stepCount: number;
@@ -6,13 +7,19 @@ interface StepIndicatorBarProps {
 }
 
 export default function StepIndicatorBar({ stepCount, currentPosition }: StepIndicatorBarProps) {
+
+    const { colorScheme } = useColorScheme();
+    const currentStepColor = colorScheme === "dark" ? "#a855f7" : "#9333ea";
+    const otherStepColor = colorScheme === "dark" ? "#262626" : "#f5f5f5";
+
     return (
         <View className="flex-row items-center justify-between px-4 pb-3">
             {Array.from({ length: stepCount + 1 }).map((_, index) => (
                 <View key={index} className="flex-row items-center flex-1 -translate-x-1/2"> {/* To make the elemnt centered */}
                     {index !== 0 && (
                         <View
-                            className={`h-0.5 flex-1 mx-2 ${index <= currentPosition ? 'bg-[#4aae4f]' : 'bg-[#aaaaaa]'}`}
+                            style={{ backgroundColor: index <= currentPosition ? currentStepColor : otherStepColor }} /* There was no other way to add colors */
+                            className="h-0.5 flex-1 mx-2"
                         />
                     )}
                 </View>
