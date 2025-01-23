d7295ffb6db24d29c2bed5b7d9e3215e19dcbc81
tomas-sedlak <t.sedlak.24@gmail.com>
2025-01-23 13:09:55 +0100

    Fixed StepIndicatorBar component

--- components/Register/RegisterHeader.tsx
+++ components/Register/RegisterHeader.tsx
@@ -16,6 +16,7 @@ export default function RegisterHeader({ stepCount, currentPosition }: RegisterH
             <StepIndicatorBar
                 stepCount={stepCount}
                 currentPosition={currentPosition}
+                className="px-4"
             />
         </View>
     )
--- components/StepIndicatorBar.tsx
+++ components/StepIndicatorBar.tsx
@@ -1,28 +1,23 @@
 import { View } from "react-native";
-import { useColorScheme } from "nativewind";
 
 interface StepIndicatorBarProps {
     stepCount: number;
     currentPosition: number;
+    className?: string;
 }
 
-export default function StepIndicatorBar({ stepCount, currentPosition }: StepIndicatorBarProps) {
-
-    const { colorScheme } = useColorScheme();
-    const currentStepColor = colorScheme === "dark" ? "#a855f7" : "#9333ea";
-    const otherStepColor = colorScheme === "dark" ? "#262626" : "#f5f5f5";
-
+export default function StepIndicatorBar({ stepCount, currentPosition, className = "" }: StepIndicatorBarProps) {
     return (
-        <View className="flex-row items-center justify-between px-4 pb-3">
-            {Array.from({ length: stepCount + 1 }).map((_, index) => (
-                <View key={index} className="flex-row items-center flex-1 -translate-x-1/2"> {/* To make the elemnt centered */}
-                    {index !== 0 && (
-                        <View
-                            style={{ backgroundColor: index <= currentPosition ? currentStepColor : otherStepColor }} /* There was no other way to add colors */
-                            className="h-0.5 flex-1 mx-2"
-                        />
-                    )}
-                </View>
+        <View className={`flex-row items-center justify-between gap-2 ${className}`}>
+            {Array.from({ length: stepCount }).map((_, index) => (
+                <View
+                    key={index}
+                    className={`h-1 flex-1 rounded-full ${
+                        index === (currentPosition - 1)
+                            ? "bg-primary-500 dark:bg-primary-400"
+                            : "bg-neutral-100 dark:bg-neutral-800"
+                    }`}
+                />
             ))}
         </View>
     );
