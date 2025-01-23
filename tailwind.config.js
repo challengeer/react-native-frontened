3f4d10543f0e9035434a4c9c3df5533c6a616395
tomas-sedlak <t.sedlak.24@gmail.com>
2025-01-23 13:09:35 +0100

    Added primary color into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#f3e8ff', // Tailwind purple 100
+          200: '#e9d5ff', // Tailwind purple 200
+          300: '#d8b4fe', // Tailwind purple 300
+          400: '#c084fc', // Tailwind purple 400
+          500: '#a855f7', // Tailwind purple 500
+          600: '#9333ea', // Tailwind purple 600
+          700: '#7e22ce', // Tailwind purple 700
+          800: '#6b21a8', // Tailwind purple 800
+          900: '#581c87', // Tailwind purple 900
+        },
+      },
+    },
   },
   plugins: [],
 }
