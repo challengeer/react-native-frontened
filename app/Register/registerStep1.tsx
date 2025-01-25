46280dffbf51c8cfe10348305d3aaa30998e3820
mark-takac <steam.omg.mk@gmail.com>
2025-01-25 12:55:44 +0100

    Formatting

--- app/Register/registerStep1.tsx
+++ app/Register/registerStep1.tsx
@@ -4,19 +4,19 @@ import InputBar from "@/components/InputBar";
 import CustomButton from "@/components/CustomButton";
 
 interface RegisterStep1Props {
-  onNext: () => void;
+    onNext: () => void;
 }
 
 export default function RegisterStep1({ onNext }: RegisterStep1Props) {
-  return (
-    <View className="flex-1 px-4">
-      <InputBar 
-        label={i18n.t("register.nameLabel")} 
-        description={i18n.t("register.legalText")} 
-      />
-      <View className="mt-auto mb-6">
-        <CustomButton title="Continue" onPress={onNext} />
-      </View>
-    </View>
-  );
+    return (
+        <View className="flex-1 px-4">
+            <InputBar
+                label={i18n.t("register.nameLabel")}
+                description={i18n.t("register.legalText")}
+            />
+            <View className="mt-auto mb-6">
+                <CustomButton title="Continue" onPress={onNext} large />
+            </View>
+        </View>
+    );
 }
