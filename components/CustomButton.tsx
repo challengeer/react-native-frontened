616abd97ff9c76c3ccdfc06daa9399cf4396fe86
mark-takac <steam.omg.mk@gmail.com>
2025-01-25 11:47:22 +0100

    Added custom button with left and right sections as well

---
+++ components/CustomButton.tsx
@@ -0,0 +1,44 @@
+import { TouchableOpacity } from "react-native";
+import Text from "@/components/Text";
+import { View } from "react-native";
+import { useColorScheme } from "nativewind";
+
+interface CustomButtonProps {
+    title: string;
+    leftSection?: React.ReactNode;
+    rightSection?: React.ReactNode;
+    large?: boolean;
+    disabled?: boolean;
+}
+
+export default function CustomButton({ title, leftSection, rightSection, large = false, disabled = false, ...props }: CustomButtonProps) {
+
+    const { colorScheme } = useColorScheme();
+    const onPress = () => {
+        console.log("Button pressed");
+    }
+
+    return (
+        <View className="px-4">
+            <TouchableOpacity
+                disabled={disabled}
+                className={[
+                    large ? "py-6 px-8" : "py-4 px-2",
+                    disabled ? "bg-gray-300" : "bg-primary-500 dark:bg-primary-400",
+                    "flex-row items-center justify-center rounded-full relative",
+                ].join(" ")}
+                onPress={() => {
+                    console.log("Button pressed");
+                }}
+                {...props}
+            >
+                {leftSection && <View className="absolute left-4">{leftSection}</View>}
+                <Text className={`${disabled ? "text-purple-500" : "text-white"} justify-self-center`}>
+                    {title}
+                </Text>
+                {rightSection && <View className="absolute right-4">{rightSection}</View>}
+            </TouchableOpacity>
+        </View>
+    )
+}
+
