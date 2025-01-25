1cf835e418f4c88b78645005b48dae9cad2849ab
mark-takac <steam.omg.mk@gmail.com>
2025-01-25 13:46:58 +0100

    Uncommented things

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,13 +1,20 @@
-// import { I18n } from "i18n-js";
-// import { getLocales } from "expo-localization";
+import { I18n } from "i18n-js";
+import { getLocales } from "expo-localization";
 
-// import en from "./locales/en.json";
-// import sk from "./locales/sk.json";
+import en from "./locales/en.json";
+import sk from "./locales/sk.json";
 
-// const translations = { en, sk };
-// const i18n = new I18n(translations);
+const translations = { en, sk };
+const i18n = new I18n(translations);
 
-// i18n.locale = getLocales()[0].languageCode ?? "en";
-// i18n.enableFallback = true;
+// Add a try-catch block and provide a fallback
+try {
+  const locales = getLocales();
+  i18n.locale = locales?.[0]?.languageCode ?? "en";
+} catch (error) {
+  i18n.locale = "en";  // Fallback to English if there's an error
+}
 
-// export default i18n;
+i18n.enableFallback = true;
+
+export default i18n;
