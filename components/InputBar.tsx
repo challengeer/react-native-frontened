4505a6ed5e4dc719a768bad2bc4f7bba64a5ba5f
tomas-sedlak <t.sedlak.24@gmail.com>
2025-01-26 19:22:01 +0100

    Made input label and description optional

--- components/InputBar.tsx
+++ components/InputBar.tsx
@@ -16,12 +16,12 @@ export default function InputBar({
 }: InputBarProps) {
     return (
         <View className={`gap-2 ${className}`}>
-            <Text className="font-medium">{label}</Text>
+            {label && <Text className="font-medium">{label}</Text>}
             <TextInput
                 className="px-4 py-3 bg-neutral-100 dark:bg-neutral-800 rounded-lg text-lg text-neutral-900 dark:text-neutral-100"
                 {...props}
             />
-            <Text type="secondary" className="text-sm">{description}</Text>
+            {description && <Text type="secondary" className="text-sm">{description}</Text>}
         </View>
     )
 }
