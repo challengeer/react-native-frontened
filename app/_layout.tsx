73c7d5a58716acd0f84976805453c328d7144c6b
tomas-sedlak <t.sedlak.24@gmail.com>
2025-02-01 23:51:24 +0100

    Moved the redirect logic into a index in the root file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,27 +4,15 @@ import "@/i18n";
 import LanguageProvider from "@/components/LanguageProvider";
 
 import { useEffect } from "react";
-import { router, Slot } from "expo-router";
+import { Slot } from "expo-router";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useColorScheme } from "nativewind";
 import { StatusBar } from "expo-status-bar";
 import * as NavigationBar from 'expo-navigation-bar';
 import Text from "@/components/Text";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
 
 export default function Root() {
     const { colorScheme, toggleColorScheme } = useColorScheme();
-
-    useEffect(() => {
-        const checkAuth = async () => {
-            const userToken = await AsyncStorage.getItem("userToken");
-            if (userToken) router.push("/challenges")
-            else router.push("/auth")
-        };
-
-        checkAuth();
-    }, [])
 
     useEffect(() => {
         NavigationBar.setBackgroundColorAsync(colorScheme === "dark" ? "#171717" : "#ffffff");
