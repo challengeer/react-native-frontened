23105bf5a7c6f66f9b7407d1e0f5718fb2763ed8
mark-takac <steam.omg.mk@gmail.com>
2025-02-02 12:59:37 +0100

    Added components to this page

--- app/(tabs)/friends.tsx
+++ app/(tabs)/friends.tsx
@@ -1,7 +1,26 @@
 import Text from "@/components/Text";
+import FriendDisplay from "@/components/friends/FriendDisplay";
+import { View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import Icon from "@/components/Icon";
+import { UserPlusIcon, XMarkIcon } from "react-native-heroicons/solid";
+import CustomAvatar from "@/components/CustomAvatar";
 
 export default function FriendsPage() {
     return (
-        <Text>Friends</Text>
+        <View>
+            <Text>Friends</Text>
+            <FriendDisplay rightSection={
+                <View className="flex-row gap-2 items-center">
+                    <CustomButton title="Add" leftSection={
+                        <Icon icon={UserPlusIcon} lightColor="white" darkColor="white" />
+                    } />
+                    <Icon icon={XMarkIcon} />
+                </View>
+            } 
+            leftSection={
+                <CustomAvatar />
+            } />
+        </View>
     )
 }
