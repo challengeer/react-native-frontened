1e0c5141eea6a80df5a9094a37b7b45c0a970f64
mark-takac <steam.omg.mk@gmail.com>
2025-02-02 14:44:26 +0100

    Added friend invite modal to header

--- components/friends/FriendsHeader.tsx
+++ components/friends/FriendsHeader.tsx
@@ -5,6 +5,7 @@ import Text from "@/components/Text";
 import { UserPlusIcon } from "react-native-heroicons/solid";
 import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 import { View } from "react-native";
+import FriendInvite from "@/components/friends/FriendInvite";
 import i18n from "@/i18n"; // Add language support later
 
 export default function FriendsHeader() {
@@ -14,23 +15,7 @@ export default function FriendsHeader() {
         }
             rightSection={
                 <View className="flex-row gap-2 items-center">
-                    <View className="relative">
-                        <View className="absolute 
-                        items-center 
-                        justify-center 
-                        left-5 
-                        bottom-6 
-                        bg-red-500 
-                        text-white 
-                        text-xs 
-                        aspect-square 
-                        rounded-full 
-                        w-5 
-                        h-5">
-                            9
-                        </View> {/* I have no idea how to make a circle responsible but if there is a lot of friend requests we'll just put 9+ there */}
-                        <IconCircle icon={UserPlusIcon} size={36} />
-                    </View>
+                    <FriendInvite />
                     <IconCircle icon={MagnifyingGlassIcon} size={36} />
                 </View>
             } />
