d4eace25e89bce4380ead6958bc495ac6c42a54e
mark-takac <steam.omg.mk@gmail.com>
2025-02-02 12:58:19 +0100

    Added props

--- components/CustomAvatar.tsx
+++ components/CustomAvatar.tsx
@@ -1,11 +1,17 @@
 import UserAvatar from "react-native-user-avatar"; //No idea wgy is there an error, it seems to work fine
 
+interface CustomAvatarProps {
+    name: string; // Used for initials, maybe change it later but I think it's good for default setting
+    large?: boolean; 
+    src?: string; // Used for image/backround
+}
 
-export default function CustomAvatar() {
+export default function CustomAvatar({ name = "", large = false, src = "" }: CustomAvatarProps) {
     return (
         <UserAvatar
-            size={40}
-            name="John Doe" //Used for initials
+            size={large ? 60 : 40}
+            name={name} 
+            src={src}
         />
     );
 }
