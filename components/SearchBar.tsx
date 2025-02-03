48aa95bc0574baf06759272c3d3ad87331f913c1
tomas-sedlak <t.sedlak.24@gmail.com>
2025-02-03 22:31:35 +0100

    Removed cancel button from search bar and added ref for focusing from outside

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,74 +4,57 @@ import { useColorScheme } from "nativewind";
 import { TextInput, View } from "react-native";
 import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 import { XCircleIcon } from "react-native-heroicons/solid";
-import Text from "@/components/Text";
 import Icon from "@/components/Icon";
 
 interface SearchBarProps {
-    onCancel?: () => void;
     onSearch?: (query: string) => void;
-    withCancel?: boolean;
+    inputRef?: React.RefObject<TextInput>;
 }
 
-export default function SearchBar({ onCancel, onSearch, withCancel = false }: SearchBarProps) {
-    const inputRef = useRef<TextInput>(null);
-    const [focused, setFocused] = useState<boolean>(false);
+export default function SearchBar({ onSearch, inputRef: externalRef }: SearchBarProps) {
+    const internalRef = useRef<TextInput>(null);
+    const inputRef = externalRef || internalRef;
     const [value, setValue] = useState<string>("");
 
     const { colorScheme } = useColorScheme();
     const placeholderColor = colorScheme === "dark" ? "#a3a3a3" : "#737373";
 
     return (
-        <View className="w-full flex-row items-center gap-4">
-            <View className="relative flex-1 flex-row items-center">
-                <View className="absolute top-3 left-4 z-10">
+        <View className="relative flex-1 flex-row items-center">
+            <View className="absolute top-3 left-4 z-10">
+                <Icon
+                    icon={MagnifyingGlassIcon}
+                    lightColor="#737373"
+                    darkColor="#a3a3a3"
+                    onPress={() => inputRef.current?.focus()}
+                />
+            </View>
+
+            <TextInput
+                ref={inputRef}
+                className="px-14 py-3 w-full bg-neutral-100 dark:bg-neutral-800 rounded-lg text-lg text-neutral-900 dark:text-neutral-100"
+                placeholderTextColor={placeholderColor}
+                placeholder={i18n.t("searchBar.placeholder")}
+                value={value}
+                onChangeText={(text) => {
+                    setValue(text);
+                    onSearch?.(text);
+                }}
+                numberOfLines={1}
+            />
+            {value && (
+                <View className="absolute top-3 right-4 z-10">
                     <Icon
-                        icon={MagnifyingGlassIcon}
+                        icon={XCircleIcon}
                         lightColor="#737373"
                         darkColor="#a3a3a3"
-                        onPress={() => inputRef.current?.focus()}
+                        onPress={() => {
+                            setValue("");
+                            onSearch?.("");
+                            inputRef.current?.blur();
+                        }}
                     />
                 </View>
-
-                <TextInput
-                    ref={inputRef}
-                    className="px-14 py-3 w-full bg-neutral-100 dark:bg-neutral-800 rounded-lg text-lg text-neutral-900 dark:text-neutral-100"
-                    placeholderTextColor={placeholderColor}
-                    placeholder={i18n.t("searchBar.placeholder")}
-                    value={value}
-                    onChangeText={(text) => {
-                        setValue(text);
-                        onSearch?.(text);
-                    }}
-                    onFocus={() => setFocused(true)}
-                    onBlur={() => setFocused(false)}
-                    numberOfLines={1}
-                />
-                {value && (
-                    <View className="absolute top-3 right-4 z-10">
-                        <Icon
-                            icon={XCircleIcon}
-                            lightColor="#737373"
-                            darkColor="#a3a3a3"
-                            onPress={() => {
-                                setValue("");
-                                onSearch?.("");
-                                inputRef.current?.blur();
-                            }}
-                        />
-                    </View>
-                )}
-            </View>
-            {focused && withCancel && (
-                <Text
-                    onPress={() => {
-                        setFocused(false);
-                        onCancel?.();
-                        inputRef.current?.blur();
-                    }}
-                >
-                    {i18n.t("searchBar.cancel")}
-                </Text>
             )}
         </View>
     )
