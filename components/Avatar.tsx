d4b613801b8286ad150461fbbf265e20a1cb76c2
tomas-sedlak <t.sedlak.24@gmail.com>
2025-02-03 21:29:01 +0100

    Added avatar background color based on name

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -7,6 +7,8 @@ interface AvatarProps {
     size?: "sm" | "md" | "lg";
 }
 
+const colors = ["bg-red-400", "bg-orange-400", "bg-amber-400", "bg-yellow-400", "bg-lime-400", "bg-green-400", "bg-emerald-400", "bg-teal-400", "bg-cyan-400", "bg-sky-400", "bg-blue-400", "bg-indigo-400", "bg-violet-400", "bg-purple-400", "bg-fuchsia-400", "bg-pink-400", "bg-rose-400", "bg-slate-400", "bg-gray-400", "bg-stone-400"];
+
 export default function Avatar({ src, name, size = "md" }: AvatarProps) {
     const sizeClasses = {
         sm: "w-12 h-12",
@@ -22,13 +24,21 @@ export default function Avatar({ src, name, size = "md" }: AvatarProps) {
             .toUpperCase()
             .slice(0, 2);
     };
+
+    function colorFromText(text: string) {
+        let charSum = 0;
+        for (let i = 0; i < text.length; i++) {
+            charSum += text.charCodeAt(i);
+        }
+        return colors[charSum % colors.length];
+    }
     
     return (
-        <View className={`${sizeClasses} rounded-full bg-neutral-100 dark:bg-neutral-800 items-center justify-center`}>
+        <View className={`${sizeClasses} ${name && colorFromText(name)} rounded-full items-center justify-center`}>
             {src ? (
                 <Image src={src} />
             ) : name && (
-                <Text className="text-lg font-medium">{getInitials(name)}</Text>
+                <Text className="text-lg text-white font-medium">{getInitials(name)}</Text>
             )}
         </View>
     )
