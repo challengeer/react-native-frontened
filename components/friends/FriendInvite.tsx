eeb69bb92786983c9160d1002114af40671a956f
tomas-sedlak <t.sedlak.24@gmail.com>
2025-02-03 21:58:43 +0100

    Added dynamic display of current requests

--- components/friends/FriendInvite.tsx
+++ components/friends/FriendInvite.tsx
@@ -7,7 +7,7 @@ import Header from '@/components/Header';
 import CustomButton from '@/components/Button';
 import Icon from '@/components/Icon';
 import Text from '@/components/Text';
-import UserItem from '../UserItem';
+import UserItem from '@/components/UserItem';
 
 interface Friend {
     user_id: number;
@@ -20,7 +20,7 @@ export default function FriendInvite() {
     const [requests, setRequests] = useState<Friend[]>([]);
 
     useEffect(() => {
-        fetch("https://challengeer.srodo.sk/users/1/requests")
+        fetch("https://challengeer.srodo.sk/users/3/requests")
             .then(res => res.json())
             .then(data => setRequests(data))
     }, [])
@@ -30,9 +30,11 @@ export default function FriendInvite() {
             {/* Friend request button for opening modal */}
             <View className="relative">
                 <IconCircle icon={UserPlusIcon} onPress={() => setIsModalVisible(true)} />
-                <Pressable onPress={() => setIsModalVisible(true)} className="absolute -top-1.5 -right-1.5 w-5 h-5 items-center justify-center bg-red-500 rounded-full">
-                    <Text className="text-white text-xs font-medium">9</Text>
-                </Pressable>
+                {requests.length > 0 &&
+                    <Pressable onPress={() => setIsModalVisible(true)} className="absolute -top-1.5 -right-1.5 w-5 h-5 items-center justify-center bg-red-500 rounded-full">
+                        <Text className="text-white text-xs font-medium">{requests.length}</Text>
+                    </Pressable>
+                }
             </View>
 
             {/* MODAL FOR FRIEND REQUESTS */}
