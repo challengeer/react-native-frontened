8cd1d463631bce598397daa5db282b6dd0c05e76
tomas-sedlak <t.sedlak.24@gmail.com>
2025-02-03 22:07:23 +0100

    Added user interface

--- app/(tabs)/friends.tsx
+++ app/(tabs)/friends.tsx
@@ -2,21 +2,16 @@ import { useEffect, useState } from "react";
 import { ScrollView, View } from "react-native";
 import { XMarkIcon } from "react-native-heroicons/outline";
 import { UserPlusIcon } from "react-native-heroicons/solid";
+import UserInterface from "@/types/UserInterface";
 import Icon from "@/components/Icon";
 import Button from "@/components/Button";
 import FriendsHeader from "@/components/friends/FriendsHeader";
 import UserItem from "@/components/UserItem";
 import Text from "@/components/Text";
 
-interface Friend {
-    user_id: number;
-    display_name: string;
-    username: string;
-}
-
 export default function FriendsPage() {
-    const [friends, setFriends] = useState<Friend[]>([]);
-    const [contacts, setContacts] = useState<Friend[]>([]);
+    const [friends, setFriends] = useState<UserInterface[]>([]);
+    const [contacts, setContacts] = useState<UserInterface[]>([]);
 
     useEffect(() => {
         fetch("https://challengeer.srodo.sk/users/2/friends")
