8675cdd5247a90be50b0d6f97dd9115153ebd8d9
tomas-sedlak <t.sedlak.24@gmail.com>
2025-02-04 15:32:17 +0100

    Added tanstack query provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,12 +7,14 @@ import { useEffect } from "react";
 import { Slot } from "expo-router";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useColorScheme } from "nativewind";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from "expo-status-bar";
 import * as NavigationBar from 'expo-navigation-bar';
 import Text from "@/components/Text";
 
 export default function Root() {
     const { colorScheme, toggleColorScheme } = useColorScheme();
+    const queryClient = new QueryClient();
 
     useEffect(() => {
         NavigationBar.setBackgroundColorAsync(colorScheme === "dark" ? "#171717" : "#ffffff");
@@ -20,12 +22,14 @@ export default function Root() {
     }, [colorScheme]);
 
     return (
-        <LanguageProvider>
-            <SafeAreaView className="flex-1 bg-white dark:bg-neutral-900">
-                <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-                <Text onPress={toggleColorScheme}>Toogle theme</Text>
-                <Slot />
-            </SafeAreaView>
-        </LanguageProvider>
+        <QueryClientProvider client={queryClient}>
+            <LanguageProvider>
+                <SafeAreaView className="flex-1 bg-white dark:bg-neutral-900">
+                    <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+                    <Text onPress={toggleColorScheme}>Toogle theme</Text>
+                    <Slot />
+                </SafeAreaView>
+            </LanguageProvider>
+        </QueryClientProvider>
     )
 };
