ff3a85262549bec96b1e44d0fdd8bf3d55dc308b
mark-takac <steam.omg.mk@gmail.com>
2025-02-08 10:36:00 +0100

    rounded and borderBottom props

--- components/settings/optionButton.tsx
+++ components/settings/optionButton.tsx
@@ -7,11 +7,18 @@ interface OptionButtonProps {
     onPress: () => void;
     rightSection?: React.ReactNode;
     className?: string;
+    borderBottom?: boolean;
+    rounded?: boolean;
 }
 
-export default function OptionButton({ title, value, onPress, rightSection, className }: OptionButtonProps) {
+export default function OptionButton({ title, value, onPress, rightSection, className, borderBottom = false, rounded = false }: OptionButtonProps) {
     return (
-        <View className={`bg-neutral-100 dark:bg-neutral-800 items-center flex-row justify-between p-5 border-b border-neutral-200 dark:border-neutral-700 ${className}`}>
+        <View className={`
+            ${className} 
+            ${borderBottom && "border-b"} 
+            ${rounded && "rounded-lg"}
+        bg-neutral-100 dark:bg-neutral-800 items-center flex-row justify-between p-5 border-neutral-200 dark:border-neutral-700 
+        `}>
             <Text>{title}</Text>
             <View className="flex-row items-center justify-between gap-2">
                 <Text>{value}</Text>
