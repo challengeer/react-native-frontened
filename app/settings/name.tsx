55cbe43c57f93bbe563b528c562e1f1b54842f7b
mark-takac <steam.omg.mk@gmail.com>
2025-02-08 11:31:16 +0100

    Added label instead of placeholder

--- app/settings/name.tsx
+++ app/settings/name.tsx
@@ -1,5 +1,4 @@
 import { View } from "react-native";
-import Text from "@/components/Text";
 import Header from "@/components/Header";
 import IconCircle from "@/components/IconCircle";
 import { ArrowLeftIcon } from "react-native-heroicons/outline";
@@ -15,7 +14,7 @@ export default function Name() {
                     onPress={() => router.back()}
                 />} />
             <View className="px-4 py-2">
-                <InputBar placeholder="Display name" />
+                <InputBar label="Display name" />
             </View>
         </View>
     );
