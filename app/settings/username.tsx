8fa04415d868076ccf9cb17ff94ce8ffb86d219f
mark-takac <steam.omg.mk@gmail.com>
2025-02-08 11:29:58 +0100

    Username page

---
+++ app/settings/username.tsx
@@ -0,0 +1,22 @@
+import { View } from "react-native";
+import Header from "@/components/Header";
+import IconCircle from "@/components/IconCircle";
+import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import { router } from "expo-router";
+import InputBar from "@/components/InputBar";
+import UsernameInput from "@/components/register/UsernameInput";
+
+export default function Username() {
+    return (
+        <View>
+            <Header title="Username" leftSection={
+                <IconCircle
+                    icon={ArrowLeftIcon}
+                    onPress={() => router.back()}
+                />} />
+            <View className="px-4 py-2">
+                <UsernameInput />
+            </View>
+        </View>
+    );
+}   
