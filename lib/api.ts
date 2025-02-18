0ae8aeceee1ac725cbf6a49e11fac457cf9aa249
tomas-sedlak <t.sedlak.24@gmail.com>
2025-02-18 13:35:58 +0100

    Rewrote enviroment variable not defined error in api.ts to be more specific

--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import * as SecureStore from 'expo-secure-store';
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
 
 // Get the API URL from environment variables
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 if (!API_URL) {
-  throw new Error('API_URL environment variable is not defined');
+  throw new Error("EXPO_PUBLIC_API_URL environment variable is not defined");
 }
 
 const api = axios.create({
@@ -15,7 +15,7 @@ const api = axios.create({
 // Add a request interceptor to add the bearer token
 api.interceptors.request.use(
   async (config) => {
-    const token = await SecureStore.getItemAsync('auth_token');
+    const token = await SecureStore.getItemAsync("auth_token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -32,13 +32,13 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    // If the error is 401 and we haven't tried to refresh the token yet
+    // If the error is 401 and we haven"t tried to refresh the token yet
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
         // Get refresh token
-        const refreshToken = await SecureStore.getItemAsync('refresh_token');
+        const refreshToken = await SecureStore.getItemAsync("refresh_token");
         
         // Call your refresh token endpoint
         const response = await axios.post(`${API_URL}/auth/refresh`, {
@@ -47,8 +47,8 @@ api.interceptors.response.use(
 
         // Store the new tokens
         const { access_token, refresh_token } = response.data;
-        await SecureStore.setItemAsync('auth_token', access_token);
-        await SecureStore.setItemAsync('refresh_token', refresh_token);
+        await SecureStore.setItemAsync("auth_token", access_token);
+        await SecureStore.setItemAsync("refresh_token", refresh_token);
 
         // Retry the original request with the new token
         originalRequest.headers.Authorization = `Bearer ${access_token}`;
