16716536fdd48be1508a4c219fd12734b4600939
tomas-sedlak <t.sedlak.24@gmail.com>
2025-03-04 10:20:36 +0100

    First commit of fetching challenges

--- app/(app)/(tabs)/challenges.tsx
+++ app/(app)/(tabs)/challenges.tsx
@@ -1,42 +1,43 @@
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, RefreshControl } from "react-native";
 import { router } from "expo-router";
 import ChallengeItem from "@/components/challenges/ChallengeItem";
 import Text from "@/components/common/Text";
 import ChallengesHeader from "@/components/challenges/ChallengesHeader";
+import api from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import { useCallback } from "react";
 
 export default function ChallengesPage() {
-    const challenges = [
-        {
-            title: "Monday run",
-            type: "Running",
-            text: "👟",
-            time: "1d left",
-            isActive: false,
+    const { data: challenges, isPending: isChallengesPending, refetch: refetchChallenges } = useQuery({
+        queryKey: ["challenges"],
+        queryFn: async () => {
+            const response = await api.get("/challenges/list");
+            return response.data;
         },
-        {
-            title: "Let's hit the gym",
-            type: "Gym",
-            text: "🏋️‍♂️",
-            time: "5d left",
-            isActive: true,
-        }
-    ]
+    });
 
-    const invites = [
-        {
-            title: "Let's hit legs",
-            type: "Gym",
-            text: "🏋️‍♂️",
-            time: "5d left",
-        }
-    ]
+    const { data: invites, isPending: isInvitesPending, refetch: refetchInvites } = useQuery({
+        queryKey: ["challenge-invites"],
+        queryFn: async () => {
+            const response = await api.get("/challenges/invites");
+            return response.data;
+        },
+    });
+
+    const refresh = useCallback(() => {
+        refetchChallenges();
+        refetchInvites();
+    }, [refetchChallenges, refetchInvites]);
 
     return (
-        <ScrollView className="flex-1 bg-white dark:bg-neutral-900">
+        <ScrollView
+        className="flex-1 bg-white dark:bg-neutral-900"
+        refreshControl={<RefreshControl refreshing={isChallengesPending || isInvitesPending} onRefresh={refresh} />}>
             <ChallengesHeader />
-            {challenges.map((challenge, index) => (
+            {challenges?.map((challenge, index) => (
                 <ChallengeItem 
-                    key={challenge.title} 
+                    key={challenge.title}
+                    time={challenge.end_date}
                     {...challenge} 
                     onPress={() => router.push("/challenge/1")} 
                     index={index} 
@@ -44,7 +45,7 @@ export default function ChallengesPage() {
             ))}
             <View className="py-3">
                 <Text className="text-left text-neutral-500 text-2xl font-bold px-4">Invites</Text>
-                {invites.map((invite, index) => (
+                {invites?.map((invite, index) => (
                     <ChallengeItem 
                         key={invite.title} 
                         {...invite} 
