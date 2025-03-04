a613c43055e0e31c1a852ba4f6790f7b2eb417f5
mark-takac <steam.omg.mk@gmail.com>
2025-03-04 09:49:15 +0100

    Added invivtes

--- app/(app)/(tabs)/challenges.tsx
+++ app/(app)/(tabs)/challenges.tsx
@@ -11,24 +11,50 @@ export default function ChallengesPage() {
             type: "Running",
             text: "👟",
             time: "1d left",
+            isActive: false,
         },
         {
             title: "Let's hit the gym",
             type: "Gym",
             text: "🏋️‍♂️",
             time: "5d left",
+            isActive: true,
         }
+    ]
 
+    const invites = [
+        {
+            title: "Let's hit legs",
+            type: "Gym",
+            text: "🏋️‍♂️",
+            time: "5d left",
+        }
     ]
 
     return (
-        <ScrollView className="flex-1  bg-white dark:bg-neutral-900">
+        <ScrollView className="flex-1 bg-white dark:bg-neutral-900">
             <ChallengesHeader />
             {challenges.map((challenge, index) => (
-                <ChallengeItem key={challenge.title} {...challenge} onPress={() => router.push("/challenge/1")} isActive index={index} />
+                <ChallengeItem 
+                    key={challenge.title} 
+                    {...challenge} 
+                    onPress={() => router.push("/challenge/1")} 
+                    index={index} 
+                />
             ))}
-            <View className="px-4 py-3">
-                <Text className="text-left text-neutral-500 text-2xl font-medium">Invites</Text>
+            <View className="py-3">
+                <Text className="text-left text-neutral-500 text-2xl font-bold px-4">Invites</Text>
+                {invites.map((invite, index) => (
+                    <ChallengeItem 
+                        key={invite.title} 
+                        {...invite} 
+                        onPress={() => router.push("/challenge/1")} 
+                        index={index}
+                        showActions
+                        onJoin={() => router.push("/challenge/1")}
+                        onCancel={() => console.log("Cancelled invite")}
+                    />
+                ))}
             </View>
         </ScrollView>
     )
