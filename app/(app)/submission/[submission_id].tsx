008aec6324d6d2b0ebaf8bc6eb7023ada28ccf43
mark-takac <steam.omg.mk@gmail.com>
2025-03-07 08:32:37 +0100

    Submission_id page

---
+++ app/(app)/submission/[submission_id].tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import Text from "@/components/common/Text";
+
+export default function SubmissionPage() {
+    return (
+        <View>
+            <Text>Submission</Text>
+        </View>
+    )
+}
