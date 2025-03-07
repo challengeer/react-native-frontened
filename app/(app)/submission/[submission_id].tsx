86bbbc4fe636db32674d4f454ffbe63280f0867c
tomas-sedlak <t.sedlak.24@gmail.com>
2025-03-07 17:58:21 +0100

    Uploading photo to server after capturing

--- app/(app)/submission/[submission_id].tsx
+++ app/(app)/submission/[submission_id].tsx
@@ -1,10 +1,27 @@
 import { View } from "react-native";
-import Text from "@/components/common/Text";
+import { useLocalSearchParams } from "expo-router";
+import { useQuery } from "@tanstack/react-query";
+import api from "@/lib/api";
+import { Image } from "expo-image";
 
 export default function SubmissionPage() {
+    const { submission_id } = useLocalSearchParams<{ submission_id: string }>();
+
+    const { data, isLoading } = useQuery({
+        queryKey: ["submission", submission_id],
+        queryFn: async () => {
+            const respone = await api.get(`/challenges/${submission_id}/submissions`);
+            console.log(respone);
+            return respone.data;
+        },
+    });
+
     return (
         <View>
-            <Text>Submission</Text>
+            <Image
+                source={{ uri: data?.[0].photo_url }}
+                style={{ width: "100%", height: "100%" }}
+            />
         </View>
     )
 }
