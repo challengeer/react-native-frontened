370f8a298a9774469aac65c4a3bf21c787d4914d
mark-takac <steam.omg.mk@gmail.com>
2025-03-10 12:20:32 +0100

    Custom header

---
+++ components/create_challenge/CreateChallengeHeader.tsx
@@ -0,0 +1,29 @@
+import { View } from "react-native";
+import Text from "@/components/common/Text";
+import StepIndicatorBar from "@/components/common/StepIndicatorBar";
+import Header from "@/components/common/Header";
+import IconCircle from "@/components/common/IconCircle";
+import { ArrowLeftIcon } from "react-native-heroicons/outline";
+
+interface CreateChallengeHeaderProps {
+    currentStep: number;
+    onBack: () => void;
+}
+
+export default function CreateChallengeHeader({ currentStep, onBack }: CreateChallengeHeaderProps) {
+    return (
+        <View>
+            <Header
+                title="Create Challenge"
+                leftSection={
+                    <IconCircle icon={ArrowLeftIcon} onPress={onBack} />
+                }
+            />
+            <StepIndicatorBar
+                stepCount={3}
+                currentPosition={currentStep}
+                className="px-4"
+            />
+        </View>
+    );
+}       
