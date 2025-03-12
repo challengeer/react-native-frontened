f7e9634be5265409b30ca7d257554ac05023eb77
tomas-sedlak <t.sedlak.24@gmail.com>
2025-03-12 22:57:26 +0100

    Playing the animation on close for submission page

--- app/(app)/submission/_layout.tsx
+++ app/(app)/submission/_layout.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo } from "react";
 import { Dimensions } from "react-native";
-import { router, Slot } from "expo-router";
+import { router, Slot, useNavigation } from "expo-router";
 import Animated, {
     useAnimatedStyle,
     useSharedValue,
@@ -18,10 +18,17 @@ const ANIMATION_CONFIG = { duration: 200 };
 export default function SubmissionLayout() {
     const scale = useSharedValue(0);
     const translateY = useSharedValue(0);
+    const navigation = useNavigation();
 
     useEffect(() => {
         scale.value = withTiming(1, ANIMATION_CONFIG);
-    }, []);
+
+        const unsubscribe = navigation.addListener("beforeRemove", (e) => {            
+            scale.value = withTiming(0, ANIMATION_CONFIG);
+        });
+
+        return unsubscribe;
+    }, [navigation]);
 
     const closeScreen = () => {
         router.back();
