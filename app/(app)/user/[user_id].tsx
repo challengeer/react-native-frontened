103ef2baee30ad12e0dc7a0a6a6e7dd714fd4ef6
tomas-sedlak <t.sedlak.24@gmail.com>
2025-03-15 13:13:01 +0100

    Added refetching to user page

--- app/(app)/user/[user_id].tsx
+++ app/(app)/user/[user_id].tsx
@@ -1,13 +1,14 @@
 import i18n from "@/i18n";
 import api from "@/lib/api";
 import { router, useLocalSearchParams } from "expo-router";
-import { ActivityIndicator, ScrollView, View } from "react-native";
+import React, { ActivityIndicator, RefreshControl, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useQuery } from "@tanstack/react-query";
 import { ArrowLeftIcon, ShareIcon, Cog8ToothIcon, CheckCircleIcon } from "react-native-heroicons/outline";
 import { UserPlusIcon, UserMinusIcon } from "react-native-heroicons/solid";
-import { useAuth } from "@/components/context/AuthProvider";
 import { FriendshipStatus } from "@/types/FriendshipTypes";
+import { useAuth } from "@/components/context/AuthProvider";
+import { useFriendActions } from '@/lib/hooks/useFriendActions';
 import UserInterface from "@/types/UserInterface";
 import Text from "@/components/common/Text";
 import Header from "@/components/common/Header";
@@ -15,7 +16,6 @@ import IconCircle from "@/components/common/IconCircle";
 import Avatar from "@/components/common/Avatar";
 import Button from "@/components/common/Button";
 import Icon from "@/components/common/Icon";
-import { useFriendActions } from '@/lib/hooks/useFriendActions';
 import ActivityCalendar from "@/components/user/ActivityCalendar"
 
 interface UserProfile extends UserInterface {
@@ -28,7 +28,7 @@ export default function UserPage() {
     const { user } = useAuth();
     const { addFriend, acceptRequest, rejectRequest } = useFriendActions();
 
-    const { data, isPending, error } = useQuery<UserProfile>({
+    const { data, isPending, isError, refetch } = useQuery<UserProfile>({
         queryKey: ["user", user_id],
         queryFn: async () => {
             const response = await api.get(`/user/${encodeURIComponent(user_id)}`);
@@ -67,58 +67,35 @@ export default function UserPage() {
                 }
             />
 
-            {isPending ? (
-                <ActivityIndicator className="justify-center py-12" size="large" color="#a855f7" />
-            ) : error ? (
-                <Text className="p-4">Error</Text>
-            ) : (
-                <ScrollView className="px-4">
-                    <View className="items-center">
-                        <Avatar size="lg" source={data.profile_picture} name={data.display_name} />
-                        <Text className="mt-2 text-2xl font-bold">{data.display_name}</Text>
-                        <Text type="secondary">@{data.username}</Text>
-                    </View>
+            <ScrollView
+                overScrollMode="never"
+                showsVerticalScrollIndicator={false}
+                className="px-4"
+                refreshControl={
+                    <RefreshControl
+                        refreshing={isPending}
+                        onRefresh={refetch}
+                    />
+                }
+            >
+                {isPending ? (
+                    <ActivityIndicator className="justify-center py-12" size="large" color="#a855f7" />
+                ) : isError ? (
+                    <Text className="p-4">Error</Text>
+                ) : (
+                    <>
+                        <View className="items-center">
+                            <Avatar size="lg" source={data.profile_picture} name={data.display_name} />
+                            <Text className="mt-2 text-2xl font-bold">{data.display_name}</Text>
+                            <Text type="secondary">@{data.username}</Text>
+                        </View>
 
-                    {data.friendship_status == "none" && user?.user_id != user_id && (
-                        <Button
-                            size="md"
-                            className="mt-6 w-full"
-                            title={i18n.t("friendActionButton.add")}
-                            onPress={() => addFriend.mutate(user_id)}
-                            leftSection={
-                                <Icon
-                                    icon={UserPlusIcon}
-                                    lightColor="white"
-                                    darkColor="white"
-                                />
-                            }
-                        />
-                    )}
-
-                    {data.friendship_status == "request_sent" && (
-                        <Button
-                            size="md"
-                            className="mt-6 w-full"
-                            variant="secondary"
-                            title={i18n.t("friendActionButton.added")}
-                            onPress={() => { }}
-                            leftSection={
-                                <Icon
-                                    icon={CheckCircleIcon}
-                                    lightColor="black"
-                                    darkColor="white"
-                                />
-                            }
-                        />
-                    )}
-
-                    {data.friendship_status == "request_received" && (
-                        <View className="mt-6 flex-row items-center gap-2">
+                        {data.friendship_status == "none" && user?.user_id != user_id && (
                             <Button
                                 size="md"
-                                className="flex-1"
-                                title={i18n.t("friendActionButton.accept")}
-                                onPress={() => data.request_id && acceptRequest.mutate(data.request_id)}
+                                className="mt-6 w-full"
+                                title={i18n.t("friendActionButton.add")}
+                                onPress={() => addFriend.mutate(user_id)}
                                 leftSection={
                                     <Icon
                                         icon={UserPlusIcon}
@@ -127,45 +104,80 @@ export default function UserPage() {
                                     />
                                 }
                             />
+                        )}
+
+                        {data.friendship_status == "request_sent" && (
                             <Button
                                 size="md"
-                                className="flex-1"
+                                className="mt-6 w-full"
                                 variant="secondary"
-                                title={i18n.t("friendActionButton.ignore")}
-                                onPress={() => data.request_id && rejectRequest.mutate(data.request_id)}
+                                title={i18n.t("friendActionButton.added")}
+                                onPress={() => { }}
                                 leftSection={
                                     <Icon
-                                        icon={UserMinusIcon}
-                                        lightColor="white"
+                                        icon={CheckCircleIcon}
+                                        lightColor="black"
                                         darkColor="white"
                                     />
                                 }
                             />
-                        </View>
-                    )}
-                    <View className="flex-row items-center gap-2 mt-6">
-                        <View className="flex-row items-center gap-2 bg-neutral-100 dark:bg-neutral-800 px-4 py-3 rounded-xl flex-1">
-                            <Text className="text-2xl">🔥</Text>
-                            <View className="flex-col">
-                                <Text className="text-xl font-bold">12 days</Text>
-                                <Text type="secondary" className="text-base">{i18n.t("user.streak")}</Text>
-                            </View>
-                        </View>
-                        <View className="flex-row items-center gap-2 bg-neutral-100 dark:bg-neutral-800 px-4 py-3 rounded-xl flex-1">
-                            <Text className="text-2xl">🎯</Text>
-                            <View className="flex-col">
-                                <Text className="text-xl font-bold">10</Text>
-                                <Text type="secondary" className="text-base">{i18n.t("user.totalChallenges")}</Text>
-                            </View>
-                        </View>
-                    </View>
+                        )}
 
-                    <Text className="text-2xl font-bold mt-6">{i18n.t("user.activity")}</Text>
-                    <ActivityCalendar
-                        selectedDates={selectedDates}
-                    />
-                </ScrollView>
-            )}
+                        {data.friendship_status == "request_received" && (
+                            <View className="mt-6 flex-row items-center gap-2">
+                                <Button
+                                    size="md"
+                                    className="flex-1"
+                                    title={i18n.t("friendActionButton.accept")}
+                                    onPress={() => data.request_id && acceptRequest.mutate(data.request_id)}
+                                    leftSection={
+                                        <Icon
+                                            icon={UserPlusIcon}
+                                            lightColor="white"
+                                            darkColor="white"
+                                        />
+                                    }
+                                />
+                                <Button
+                                    size="md"
+                                    className="flex-1"
+                                    variant="secondary"
+                                    title={i18n.t("friendActionButton.ignore")}
+                                    onPress={() => data.request_id && rejectRequest.mutate(data.request_id)}
+                                    leftSection={
+                                        <Icon
+                                            icon={UserMinusIcon}
+                                            lightColor="white"
+                                            darkColor="white"
+                                        />
+                                    }
+                                />
+                            </View>
+                        )}
+                        <View className="flex-row items-center gap-2 mt-6">
+                            <View className="flex-row items-center gap-2 bg-neutral-100 dark:bg-neutral-800 px-4 py-3 rounded-xl flex-1">
+                                <Text className="text-2xl">🔥</Text>
+                                <View className="flex-col">
+                                    <Text className="text-xl font-bold">12 days</Text>
+                                    <Text type="secondary" className="text-base">{i18n.t("user.streak")}</Text>
+                                </View>
+                            </View>
+                            <View className="flex-row items-center gap-2 bg-neutral-100 dark:bg-neutral-800 px-4 py-3 rounded-xl flex-1">
+                                <Text className="text-2xl">🎯</Text>
+                                <View className="flex-col">
+                                    <Text className="text-xl font-bold">10</Text>
+                                    <Text type="secondary" className="text-base">{i18n.t("user.totalChallenges")}</Text>
+                                </View>
+                            </View>
+                        </View>
+
+                        <Text className="text-2xl font-bold mt-6">{i18n.t("user.activity")}</Text>
+                        <ActivityCalendar
+                            selectedDates={selectedDates}
+                        />
+                    </>
+                )}
+            </ScrollView>
         </SafeAreaView>
     )
 }
