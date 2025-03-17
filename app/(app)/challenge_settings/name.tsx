4a234d94f785f03fa0f81bf31cf3fde292b1e712
mark-takac <steam.omg.mk@gmail.com>
2025-03-17 21:58:54 +0100

    Challenge setting page for changing name

---
+++ app/(app)/challenge_settings/name.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import Text from "@/components/common/Text";
+export default function Name() {
+    return (
+        <View>
+            <Text>Name</Text>
+        </View>
+    )
+}
