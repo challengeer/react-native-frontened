c6d18aa6e1f2dcc75bb9c51291d7bf5632912a21
mark-takac <steam.omg.mk@gmail.com>
2025-03-17 21:56:45 +0100

    Wrapped into SafeAreaView, added settings buttons with routes using settingItem component

--- app/(app)/challenge_settings/index.tsx
+++ app/(app)/challenge_settings/index.tsx
@@ -1,14 +1,43 @@
-import { View } from "react-native";
+import { View, ScrollView, SafeAreaView } from "react-native";
 import Text from "@/components/common/Text";
 import Header from "@/components/common/Header";
 import IconCircle from "@/components/common/IconCircle";
+import SettingsItem from "@/components/settings/SettingsItem";
 import { ArrowLeftIcon } from "react-native-heroicons/outline";
 import { router } from "expo-router";
 export default function ChallengeSettings() {
+
+    const settingsItems = [
+        {
+            title: "Name",
+            onPress: () => router.push("/challenge_settings/name"),
+        },
+        {
+            title: "Description",
+            onPress: () => router.push("/challenge_settings/description"),
+        },
+        {
+            title: "Participants",
+            onPress: () => router.push("/challenge_settings/participants"),
+        },
+    ]
     return (
-        <View>
+        <SafeAreaView className="flex-1">
             <Header title="Challenge Settings" leftSection={<IconCircle icon={ArrowLeftIcon} onPress={() => router.back()} />} />
-            <Text>Challenge Settings</Text>
-        </View>
+            <ScrollView className="px-4">
+                <View className="mb-4">
+                    {settingsItems.map((item, index) => (
+                        <SettingsItem
+                            key={index}
+                            itemIndex={index}
+                            totalItems={settingsItems.length}
+                            title={item.title}
+                            onPress={item.onPress}
+                            showArrow
+                        />
+                    ))}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
