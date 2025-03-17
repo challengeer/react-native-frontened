120a2a58adc74451d587f6c52e9bd400937a6548
mark-takac <steam.omg.mk@gmail.com>
2025-03-17 22:07:54 +0100

    Multilanguage support, added textarea with maxLength for challenge discription setting page

--- app/(app)/challenge_settings/description.tsx
+++ app/(app)/challenge_settings/description.tsx
@@ -1,9 +1,50 @@
-import { View } from "react-native";
-import Text from "@/components/common/Text";
+import i18n from "@/i18n";
+import { router } from "expo-router";
+import { useState } from "react";
+import { SafeAreaView, View } from "react-native";
+import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import { TextAreaInputBar } from "@/components/common/InputBar";
+import Header from "@/components/common/Header";
+import IconCircle from "@/components/common/IconCircle";
+import Button from "@/components/common/Button";
+
 export default function Description() {
-  return (
-    <View>
-      <Text>Description</Text>
-    </View>
-  );
+    const [description, setDescription] = useState("");
+    const [isValidDescription, setIsValidDescription] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = async () => {
+        setIsLoading(true);
+    }
+
+    return (
+        <SafeAreaView className="flex-1">
+            <Header
+                title={i18n.t("challenge_settings.description.header")}
+                leftSection={
+                    <IconCircle
+                        icon={ArrowLeftIcon}
+                        onPress={() => router.back()}
+                    />
+                }
+            />
+
+            <View className="flex-1 px-4 pb-4 justify-between">
+                <TextAreaInputBar
+                    value={description}
+                    onChangeText={setDescription}
+                    description={i18n.t("challenge_settings.description.description")}
+                    maxLength={500}
+                    autoFocus
+                />
+                <Button
+                    title={i18n.t("buttons.save")}
+                    size="lg"
+                    disabled={!isValidDescription || isLoading}
+                    loading={isLoading}
+                    onPress={handleSubmit}
+                />
+            </View>
+        </SafeAreaView>
+    );
 }
