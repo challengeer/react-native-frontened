5775fe84dbc779cd8a2f14082d83de91b616a1d7
mark-takac <steam.omg.mk@gmail.com>
2025-03-17 21:43:54 +0100

    Added challenge description setting page

---
+++ app/(app)/challenge_settings/description.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import Text from "@/components/common/Text";
+export default function Description() {
+  return (
+    <View>
+      <Text>Description</Text>
+    </View>
+  );
+}
