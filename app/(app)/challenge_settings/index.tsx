a891fec83e354536ae21e54afc5da8dd94877ece
mark-takac <steam.omg.mk@gmail.com>
2025-03-17 21:50:29 +0100

    Header with back button

--- app/(app)/challenge_settings/index.tsx
+++ app/(app)/challenge_settings/index.tsx
@@ -1,9 +1,13 @@
 import { View } from "react-native";
 import Text from "@/components/common/Text";
-
+import Header from "@/components/common/Header";
+import IconCircle from "@/components/common/IconCircle";
+import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import { router } from "expo-router";
 export default function ChallengeSettings() {
     return (
         <View>
+            <Header title="Challenge Settings" leftSection={<IconCircle icon={ArrowLeftIcon} onPress={() => router.back()} />} />
             <Text>Challenge Settings</Text>
         </View>
     )
