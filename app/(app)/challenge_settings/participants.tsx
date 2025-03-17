44f55a01123f58e09a2de14e768091967b63e5b6
mark-takac <steam.omg.mk@gmail.com>
2025-03-17 21:43:21 +0100

    Added participants settings page

---
+++ app/(app)/challenge_settings/participants.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import Text from "@/components/common/Text";
+
+
+export default function Participants() {
+    return (
+        <View>
+            <Text>Participants</Text>
+        </View>
+    );
+}
