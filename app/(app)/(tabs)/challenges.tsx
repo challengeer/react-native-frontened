74a2b942397dc9580bc29bddcd92cfbccc4a1c86
tomas-sedlak <t.sedlak.24@gmail.com>
2025-05-18 19:24:45 +0200

    Removed unused imports from challenges page

--- app/(app)/(tabs)/challenges.tsx
+++ app/(app)/(tabs)/challenges.tsx
@@ -1,43 +1,11 @@
-import i18n from "@/i18n";
-import api from "@/lib/api";
-import React, { useCallback } from "react";
-import { View, ScrollView, RefreshControl, ActivityIndicator } from "react-native";
-import { router } from "expo-router";
-import { useQuery } from "@tanstack/react-query";
-import { ChallengeSimple } from "@/types/Challenge";
-import { PlusIcon, UserGroupIcon } from "react-native-heroicons/outline";
-import ChallengeItem from "@/components/challenges/ChallengeItem";
-import Text from "@/components/common/Text";
+import React from "react";
 import ChallengesHeader from "@/components/challenges/ChallengesHeader";
-import UserInterface from "@/types/UserInterface";
-import Button from "@/components/common/Button";
-import Icon from "@/components/common/Icon";
-import NetworkErrorContainer from "@/components/common/NetworkErrorContainer";
-import ChallengeRightSection from "@/components/challenges/ChallengeRightSection";
-import ChallengeInviteRightSection from "@/components/challenges/ChallengeInviteRightSection";
 import ChallengesList from "@/components/challenges/ChallengesList";
 
-interface Invitation extends ChallengeSimple {
-    sender: UserInterface;
-    invitation_id: string;
-}
-
-interface ChallengesResponse {
-    owned_challenges: ChallengeSimple[];
-    participating_challenges: ChallengeSimple[];
-    invitations: Invitation[];
-}
-
-interface Friend extends UserInterface {
-    mutual_streak: number;
-}
-
 export default function ChallengesPage() {
-
     return (
         <>
             <ChallengesHeader />
-
             <ChallengesList />
         </>
     )
