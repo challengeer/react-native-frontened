a19935b73883092e8e248c763a498839b7a4f64e
tomas-sedlak <t.sedlak.24@gmail.com>
2025-05-29 09:58:14 +0200

    Removed overscroll from tabs

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -69,6 +69,7 @@ export default function Layout() {
                 renderTabBar={renderTabBar}
                 swipeEnabled={true}
                 tabBarPosition="bottom"
+                overScrollMode="never"
                 lazy={true}
             />
         </View>
